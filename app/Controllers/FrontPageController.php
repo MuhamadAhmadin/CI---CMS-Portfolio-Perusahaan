ed971a7df7cc950f924026eab2a2a55cdd3ff4b5
Muhamad Ahmadin <ahmadinations@gmail.com>
2022-01-24 17:45:10 +0700

    feat: show the clients on homepage

--- app/Controllers/FrontPageController.php
+++ app/Controllers/FrontPageController.php
@@ -3,10 +3,11 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\Client;
 use App\Models\User;
-use App\Models\Dokter;
 use App\Models\Kamar;
 use App\Models\Pasien;
+use App\Models\Tim;
 
 class FrontPageController extends BaseController
 {
@@ -19,16 +20,20 @@ class FrontPageController extends BaseController
 
     public function homepage()
     {
+        $client = new Client();
         $user = new User();
         $pasien = new Pasien();
-        $dokter = new Dokter();
+        // $tim = new Tim();
         $kamar = new Kamar();
         $total_pendapatan = $pasien->get_total_pendapatan()->getResult();
         $data['jumlah_user'] = $user->countAll();
         $data['jumlah_pasien'] = $pasien->countAll();
-        $data['jumlah_dokter'] = $dokter->countAll();
+        // $data['jumlah_tim'] = $tim->countAll();
         $data['jumlah_kamar'] = $kamar->countAll();
         $data['total_pendapatan'] = $total_pendapatan[0]->total;
+
+        $data['clients'] = $client->asObject()->findAll();
+
         return view('front/homepage', $data);
     }
 }
